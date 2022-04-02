9e0010cc52e815dcde0533c01cf09c48b298d0ae
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-02 21:07:06 +0530

    Added: Ability to add new contact in address book logic

--- AddressBook.js
+++ AddressBook.js
@@ -1,2 +1,17 @@
 
 console.log("...Welcome to address book problem...");
+
+const Person = require("./Person.js");
+const prompt = require('prompt-sync')();
+
+//to add person details in address book
+var add = new Person();
+add.firstName = prompt("Enter First Name : ");
+add.lastName = prompt("Enter Last Name : ");
+add.address = prompt("Enter Address : ");
+add.city = prompt("Enter City : ");
+add.state = prompt("Enter State : ");
+add.zip = prompt("Enter Zip : ");
+add.phoneNumber = prompt("Enter Phone Number : ");
+add.email = prompt("Enter Email : ");
+console.log(add.toString());
